c1e3e50267e3a66fc70b0e78c3fb01309066e5f2
Білецький Данило <kirikoshamain@gmail.com>
2024-03-03 19:46:53 +0100

    Fixed task 2.1

--- PZ2_OperatingSystems/pz2_solution.cpp
+++ PZ2_OperatingSystems/pz2_solution.cpp
@@ -3,7 +3,8 @@
 #include <numeric>
 
 void task2_1();
-
+int getTheNumberOfElementsBelowAverage(int average, int length, int arr[]);
+void selectionSort(int arr[], int length);
 int main() {
 	task2_1();
 	return 0;
@@ -15,48 +16,56 @@ void task2_1() {
 	using namespace std;
 
 	//TASK 1----------------------------------
-	int arraySize;
-	cout << "Enter an array size: ";
-	cin >> arraySize;
-	int *arr = new int[arraySize];
+	const int MAX_SIZE = 100;
+	int arr [MAX_SIZE];
+	int actualSize;
+	cout << "Enter an array size that is < " << MAX_SIZE << " : ";
+	cin >> actualSize;
 
-	for (int i = 0; i < arraySize; i++) {
+	for (int i = 0; i < actualSize; i++) {
 		arr[i] = rand() % 100 + 1;
 	}
 
 	//TASK 2----------------------------------
-	for (int i = 0; i < arraySize; i++) {
+	for (int i = 0; i < actualSize; i++) {
 		cout << arr[i] << endl;
 	}
 
 	//TASK 3----------------------------------
 	int avg = 0;
-	avg = accumulate(arr, arr + arraySize, avg) / arraySize;
+	avg = accumulate(arr, arr + actualSize, avg) / actualSize;
 	cout << "Average is " << avg << endl;
-	
-	int newArrSize = 0;
-	for (int i = 0; i < arraySize; i++) {
-		if (arr[i] <= avg) {
-			newArrSize++;
+	selectionSort(arr, actualSize);
+	actualSize = getTheNumberOfElementsBelowAverage(avg, actualSize, arr);
+
+	//TASK 4----------------------------------
+	cout << "Array after the delete: " << endl;
+	for (int i = 0; i < actualSize; i++) {
+		cout << arr[i] << endl;
+	}
+}
+
+int getTheNumberOfElementsBelowAverage(int average, int length, int arr[]) {
+	int count = 0;
+	for (int i = 0; i < length; i++) {
+		if (arr[i] <= average) {
+			count++;
 		}
 	}
 
-	int* newArr = new int[newArrSize];
-	int i_arr = 0;
-	int i_newArr = 0;
-	while (i_arr < arraySize) {
-		if (arr[i_arr] <= avg) {
-			newArr[i_newArr] = arr[i_arr];
-			i_newArr++;
+	return count;
+}
+
+void selectionSort(int arr[], int length) {
+
+	for (int i = 1; i < length; i++) {
+		int j = i - 1;
+		int key = arr[i];
+
+		while (arr[j] > key && j >= 0) {
+			arr[j + 1] = arr[j];
+			j--;
 		}
-		i_arr++;
-	}
-
-	delete arr;
-
-	//TASK 4
-	cout << "New array after the delete of integers > the average" << endl;
-	for (int i = 0; i < newArrSize; i++) {
-		cout << newArr[i] << endl;
+		arr[j + 1] = key;
 	}
 }
