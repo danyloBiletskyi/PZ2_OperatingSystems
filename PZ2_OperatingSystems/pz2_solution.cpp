e6f690f58c4936362fec414f934ae9232d8379ba
Білецький Данило <kirikoshamain@gmail.com>
2024-03-06 17:41:46 +0100

    completed pz

--- PZ2_OperatingSystems/pz2_solution.cpp
+++ PZ2_OperatingSystems/pz2_solution.cpp
@@ -20,7 +20,6 @@ void Task2_2();
 void selectionSortForDouble(double arr[], int length);
 int binarySearch(double arr[], int length, double element);
 int main() {
-	/*task2_1();*/
 	Task2_2();
 	return 0;
 }
@@ -255,3 +254,290 @@ int binarySearch(double arr[], int length, double element) {
 	return left;
 	
 }
+
+
+// ConsoleApplication4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//
+// TASK 2.3
+
+
+//#include <iostream>
+//#include <cstdlib> 
+//#include <ctime>  
+//#include <climits> 
+//using namespace std;
+//
+//
+//int findMinPositiveOnSecondaryDiagonal(int matrix[][100], int n) {
+//    int minPositive = INT_MAX;
+//
+//    for (int i = 0; i < n; i++) {
+//        if (matrix[i][n - i - 1] > 0 && matrix[i][n - i - 1] < minPositive) {
+//            minPositive = matrix[i][n - i - 1];
+//        }
+//    }
+//
+//    return minPositive;
+//}
+//
+//int main() {
+//    int n;
+//
+//
+//    cout << "Enter the size of the square matrix: ";
+//    cin >> n;
+//
+//
+//    srand(time(nullptr));
+//
+//    int matrix[100][100];
+//
+//
+//    cout << "Matrix:" << endl;
+//    for (int i = 0; i < n; i++) {
+//        for (int j = 0; j < n; j++) {
+//            matrix[i][j] = rand() % 201 - 100; 
+//            cout << matrix[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//
+//    int minPositive = findMinPositiveOnSecondaryDiagonal(matrix, n);
+//
+//
+//    if (minPositive != INT_MAX) {
+//        cout << "The minimum positive number on the side diagonal: " << minPositive << endl;
+//    }
+//    else {
+//        cout << "There are no positive numbers on the side diagonal." << endl;
+//    }
+//
+//    return 0;
+//}
+//TASK 2.5(1) 
+
+//#include <iostream>
+//#include <string>
+//#include <sstream>
+//
+//int countWords(const std::string& str) {
+//    std::istringstream iss(str);
+//    std::string word;
+//    int count = 0;
+//
+//    while (iss >> word) {
+//        ++count;
+//    }
+//
+//    return count;
+//}
+//
+//int main() {
+//    std::string input;
+//
+//    std::cout << "Enter a string: ";
+//    std::getline(std::cin, input);
+//
+//    int wordCount = countWords(input);
+//    std::cout << "Count of words in string: " << wordCount << std::endl;
+//
+//    return 0;
+//}
+
+
+
+
+//
+// TASK 2.5(2)
+
+
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//
+//void stringMatrix(const std::vector<std::string>& strings, int rows) {
+//    int cols = strings.size() / rows;
+//    int index = 0;
+//
+//    for (int i = 0; i < rows; ++i) {
+//        for (int j = 0; j < cols; ++j) {
+//            const std::string& str = strings[index];
+//            for (char c : str) {
+//                std::cout << static_cast<int>(c) << ' ';
+//            }
+//            std::cout << "| ";
+//            ++index;
+//        }
+//        std::cout << "\n";
+//    }
+//}
+//
+//int main() {
+//    int n;
+//    std::cout << "Enter a number of string: ";
+//    std::cin >> n;
+//
+//    std::vector<std::string> strings;
+//    std::string input;
+//
+//    std::cout << "Enter a string: " << std::endl;
+//    std::cin.ignore();
+//    for (int i = 0; i < n; ++i) {
+//        std::getline(std::cin, input);
+//        strings.push_back(input);
+//    }
+//
+//    std::cout << "Symbol Matrix(ASCII code):" << std::endl;
+//    stringMatrix(strings, n);
+//
+//    return 0;
+//}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+
+//#include <iostream>
+//#include <cstdlib> 
+//#include <ctime>
+//
+//using namespace std;
+//
+//int main() {
+//    using namespace std;
+//    srand(time(nullptr)); 
+//
+//    int n;
+//    cout << "Enter an array size: ";
+//    cin >> n;
+//
+//    int* arr = new int[n]; 
+//
+//
+//    for (int i = 0; i < n; ++i) {
+//        arr[i] = rand() % 100; 
+//    }
+//
+//
+//    cout << "Array at the begining: ";
+//    for (int i = 0; i < n; ++i) {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//
+//    int newSize = 0;
+//    for (int i = 0; i < n; ++i) {
+//        if (arr[i] % 2 == 0) {
+//            arr[newSize++] = arr[i];
+//        }
+//    }
+//
+//
+//    cout << "After odd elements were deleted: ";
+//    for (int i = 0; i < newSize; ++i) {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//
+//    delete[] arr;
+//
+//    return 0;
+//}
+
+//
+//#include <iostream>
+//#include <cstdlib>
+//#include <ctime> 
+//
+//
+//
+//int main() {
+//    using namespace std;
+//    srand(time(nullptr));
+//
+//    int rows, cols;
+//    cout << "Enter the number of rows: ";
+//    cin >> rows;
+//    cout << "Enter the number of columns: ";
+//    cin >> cols;
+//
+//
+//    int** matrix = new int* [rows];
+//    for (int i = 0; i < rows; ++i) {
+//        matrix[i] = new int[cols];
+//    }
+//
+//
+//    for (int i = 0; i < rows; ++i) {
+//        for (int j = 0; j < cols; ++j) {
+//            matrix[i][j] = rand() % 100;
+//        }
+//    }
+//
+//
+//    cout << "Matrix at the begining:" << endl;
+//    for (int i = 0; i < rows; ++i) {
+//        for (int j = 0; j < cols; ++j) {
+//            cout << matrix[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//
+//    int k;
+//    cout << "Enter the K number of rows to add:  ";
+//    cin >> k;
+//
+//    int newRows = rows + k;
+//    int** newMatrix = new int* [newRows];
+//
+//
+//    for (int i = 0; i < newRows; ++i) {
+//        newMatrix[i] = new int[cols];
+//        if (i < k) {
+//
+//            for (int j = 0; j < cols; ++j) {
+//                newMatrix[i][j] = rand() % 100;
+//            }
+//        }
+//        else {
+//
+//            for (int j = 0; j < cols; ++j) {
+//                newMatrix[i][j] = matrix[i - k][j];
+//            }
+//        }
+//    }
+//
+//
+//    cout << "Final matrix:" << endl;
+//    for (int i = 0; i < newRows; ++i) {
+//        for (int j = 0; j < cols; ++j) {
+//            cout << newMatrix[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    for (int i = 0; i < rows; ++i) {
+//        delete[] matrix[i];
+//    }
+//    delete[] matrix;
+//
+//    for (int i = 0; i < newRows; ++i) {
+//        delete[] newMatrix[i];
+//    }
+//    delete[] newMatrix;
+//
+//    return 0;
+//}
